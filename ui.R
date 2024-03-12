e698943438731832dbebc5b0d93dad0f1857ad13
Russell Scherer <118118101+Russarevok@users.noreply.github.com>
2024-03-12 10:02:00 -0700

    Added better user instructions.

--- ui.R
+++ ui.R
@@ -13,11 +13,11 @@ fluidPage(
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            radioButtons("ArrestCategory", "Crime committed:",
+            radioButtons("ArrestCategory", "1. Choose a crime:",
                          c("Murder" = "Murder",
                            "Assault" = "Assault",
                            "Rape" = "Rape")),
-            numericInput(inputId="numericTopN", label = "Show me the top:", min=1, max=numerOfStates, value = 10),
+            numericInput(inputId="numericTopN", label = "2. Enter the top number of states to display:", min=1, max=numerOfStates, value = 10),
             # submitButton('Submit')
         ),
 
